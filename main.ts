888de7eabda298ad12bbc4178ad039724c61f2ad
MichaelaMilne <122547976+MichaelaMilne@users.noreply.github.com>
2023-01-13 11:23:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,43 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Happy)
+    music.playMelody("C D E G - - - - ", 120)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . # # # #
+        . . # # #
+        . . . # #
+        . . # # #
+        . # # # #
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Heart)
+    music.ringTone(988)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    music.ringTone(131)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Angry)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Confused)
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        # # # # .
+        # # . . .
+        # . . . .
+        # # . . .
+        # # # # .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    music.playMelody("C5 G D C - - - - ", 120)
 })
 basic.showIcon(IconNames.Asleep)
