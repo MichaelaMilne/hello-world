b5a16e20d40cb83fe928cb45fe42a9cdf141810e
MichaelaMilne <122547976+MichaelaMilne@users.noreply.github.com>
2023-01-13 11:10:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.No)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Angry)
